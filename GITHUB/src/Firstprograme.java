2994c69d567c3094e158bd3d76e07fae65c2d145
AlifiyaSaifee <Sayedna5253>
2014-09-27 17:07:25 -0700

    i have changed

---
+++ GITHUB/src/Firstprograme.java
@@ -0,0 +1,11 @@
+
+public class Firstprograme {
+
+	public static void main(String[] args) {
+		System.out.println("now clicking on English");
+		System.out.println("now clicking on japanese");
+		System.out.println("now clicking on french");
+
+	}
+
+}
---
+++ GITHUB/src/Secondprograme.java
@@ -0,0 +1,11 @@
+
+public class Secondprograme {
+
+	public static void main(String[] args) {
+		System.out.println("now trying to speak English");
+		System.out.println("now trying to speak japanese");
+		System.out.println("now trying to speak french");
+
+	}
+
+}
